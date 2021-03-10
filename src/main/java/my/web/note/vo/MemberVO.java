698ae4f38c78eb7fb7fe129b81f1f4a1ee2a9ac8
DESKTOP-41NJH0T\kwk80 <kwk8002@naver.com>
2021-03-10 13:44:54 +0900

    First Commit

---
+++ src/main/java/my/web/note/vo/MemberVO.java
@@ -0,0 +1,12 @@
+package my.web.note.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+
+		private int member_no;
+		private String member_id;
+		private String member_pw;
+		private String member_nm;
+}
